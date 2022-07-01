77aae5e41050156f7c4597c7bee7d5618fe658c3
desantojohnny <johnny.santo@gmail.com>
2022-07-01 19:21:12 -0300

    primeira-aula-react

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+/* import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -21,5 +21,30 @@ function App() {
     </div>
   );
 }
+export default App; */
+import Item from "./components/item"; //importando o arquivo 
+import Card from "./components/card"; //importando o card
 
+const App = () => {
+  return(
+    <>
+    <h1>Minha primeira aplicação</h1>
+    <ul>
+        {/* <Item texto="Primeiro"/> 
+        <Item texto="Segundo"/>
+        <Item texto=""/> --rodando props.texto */}
+        <Item>
+          Primeiro
+        </Item>
+        <Item>
+          Segundo
+        </Item>
+        <Item>
+          Terceiro
+        </Item>
+    </ul>
+    <Card/>
+    </>
+  )
+}
 export default App;
---
+++ src/components/item/index.js
@@ -0,0 +1,9 @@
+const Item = /* (props) */ ({children}) => {
+    return (
+        <a href="/" className="list-group-item list-group-item-dark">
+        {/* {props.texto}  */ children}
+        </a>
+    )
+}
+
+export default Item;
